30bcfbf95732e0fee6cc01576d6c437936bdcb09
davis <davis@Portatil_David.mshome.net>
2022-05-06 10:54:29 +0200

    Calculadora hecha

--- RepasoED/src/principal/Calculadora.java
+++ RepasoED/src/principal/Calculadora.java
@@ -1,9 +1,66 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Introducir una nueva opcion para calcular el IVA
+		// El primer numero seria el precio y el segundo el IVA
+		// El usuario introduce el IVA que desea
+
+		Scanner entrada = new Scanner(System.in);
+		
+		String eleccion = "";
+		char opcion = ' ';
+		int num1=0, num2=0;
+		boolean parar = false;
+		
+		do {
+			System.out.println("Introduce la operacion que deseas"
+					+ "\n 1) Sumar"
+					+ "\n 2) Restar"
+					+ "\n 3) Multiplicar"
+					+ "\n 4) Dividir"
+					+ "\n 5) Salir");
+					
+			opcion = entrada.nextLine().toUpperCase().charAt(0);
+			
+			if(opcion!='5') {
+				System.out.println("Introduce el primero numero");
+				num1 = Integer.parseInt(entrada.nextLine());
+				
+				System.out.println("Introduce el segundo numero");
+				num2 = Integer.parseInt(entrada.nextLine());
+			}
+			
+			switch(opcion) {
+				case '1':
+					System.out.println("Resultado: " + (num1+num2));
+					break;
+				case '2':
+					System.out.println("Resultado: " + (num1-num2));
+					break;
+				case '3':
+					System.out.println("Resultado: " + (num1*num2));
+					break;
+				case '4':
+					System.out.println("Resultado: " + (num1/num2));
+					break;
+				case '5':
+					System.out.println("Fin de programa");
+					parar = true;
+					break;
+				default: 
+					System.out.println("Opcion invalida");
+					break;
+			}
+			
+		}while(!parar);	
+				
+
+		
 
 	}
 
